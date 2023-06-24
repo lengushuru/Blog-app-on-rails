5e41c7e261c26f69b5edcc802e7663639e5d1fb7
lengushuru <charleslengushuru@gmail.com>
2023-06-25 00:32:02 +0300

    Fix test error

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,17 +3,17 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: :post_id
   has_many :likes, class_name: 'Like', foreign_key: :post_id
 
-  after_create :update_user_posts_counter
-
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counters, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def update_user_posts_counter
-    author.increment!(:post_counter)
-  end
+  after_create :update_user_posts_counter
 
   def latest_commments
-    comments.order(create_at: :desc).limit(5)
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_user_posts_counter
+    author.increment!(:posts_counter)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_24_192244) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_24_204323) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_24_192244) do
 
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id", null: false
-    t.string "title"
+    t.string "title", null: false
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counters"
