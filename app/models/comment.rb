742d84886120f3112c968eacbbf3db49aa5efb65
lengushuru <charleslengushuru@gmail.com>
2023-06-24 16:14:42 +0300

    Remove redundancy in foreign key declaration.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :author, class_name: 'User'
 
   attribute :text, :text
   after_create :update_post_comments_counter
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  belongs_to :post, class_name: 'Post', foreign_key: :post_id
-  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
+ belongs_to :author, class_name: 'User'
 
   after_create :update_post_likes_counter
 
