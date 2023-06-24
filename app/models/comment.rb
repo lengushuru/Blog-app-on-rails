61d0615011763049a2935fb443b7419f1734a6e9
lengushuru <charleslengushuru@gmail.com>
2023-06-24 15:27:41 +0300

    Add comment class

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
-end
+    belongs_to :post
+    belongs_to :author, class_name: 'User', foreign_key: :author_id
+  
+    attribute :text, :text
+    after_create :update_post_comments_counter
+  
+  end  
