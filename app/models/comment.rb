c50ac22087ad05707ef50c177b2fa3408b5efd34
lengushuru <charleslengushuru@gmail.com>
2023-06-24 15:44:44 +0300

    Fix linter error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :post
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
-  
-    attribute :text, :text
-    after_create :update_post_comments_counter
-    def update_post_comments_counter
-        post.increment!(:comments_counter)
-    end
-  end  
+  belongs_to :post
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+  attribute :text, :text
+  after_create :update_post_comments_counter
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
+  end
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,11 +1,10 @@
 class Like < ApplicationRecord
-    belongs_to :post, class_name: 'Post', foreign_key: :post_id
-    belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post, class_name: 'Post', foreign_key: :post_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
 
-    after_create :update_post_likes_counter
+  after_create :update_post_likes_counter
 
-    def update_post_likes_counter
-        post.increment!(:likes_counter)
-    end
+  def update_post_likes_counter
+    post.increment!(:likes_counter)
+  end
 end
-
