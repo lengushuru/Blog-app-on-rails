3a2dca00b27ad0c50045cb25c0a8e0b66a295cee
lengushuru <charleslengushuru@gmail.com>
2023-06-24 15:30:12 +0300

    Add like class

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,7 @@
 class Like < ApplicationRecord
+    belongs_to :post, class_name: 'Post', foreign_key: :post_id
+    belongs_to :author, class_name: 'User', foreign_key: :author_id
+
+    after_create :update_post_likes_counter
 end
+
