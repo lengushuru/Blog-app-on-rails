c8db04fb3b833f5bd80c08c9c877c477bf23b9d3
lengushuru <charleslengushuru@gmail.com>
2023-06-24 15:34:46 +0300

    Add class Users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
-end
+    has_many :post, class_name: 'Post', foreign_key: :author_id
+    has_many :comments, class_name: 'Comment', foreign_key: :author_id
+    has_many :likes, class_name: 'Like', foreign_key: :author_id
+  
+  end
