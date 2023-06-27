60d7c69845a5e03b965029aad128f99c53331418
lengushuru <charleslengushuru@gmail.com>
2023-06-27 22:18:09 +0300

    Add users tests.

---
+++ spec/requests/users_controller_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Users', type: :request do
+  describe 'GET /index' do
+    before(:each) do
+      get '/users'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+    it 'return 200' do
+      expect(response.status).to eq(200)
+    end
+    it 'renders the index template' do
+      expect(response).to render_template('index')
+    end
+    it 'include the list of users' do
+      expect(response.body).to include('All Users')
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) do
+      get '/users/1'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      expect(response).to render_template('show')
+    end
+
+    it 'includes the details of a user' do
+      expect(response.body).to include('User')
+    end
+  end
+end
