c3dbf0c0d28db4064588dfd60a5d6aeb0cf360e1
lengushuru <charleslengushuru@gmail.com>
2023-06-30 09:29:05 +0300

    Updatte post and users models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, foreign_key: 'author_id'
   has_many :comments, class_name: 'Comment', foreign_key: :author_id
   has_many :likes, class_name: 'Like', foreign_key: :author_id
 
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def latest_posts
-    post.order(created_at: :desc).limit(3)
+    posts.order(created_at: :desc).limit(3)
   end
 end
