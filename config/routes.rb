173eafd66f090ddf9ba3c0eee5b22265143dce90
lengushuru <charleslengushuru@gmail.com>
2023-07-01 16:14:34 +0300

    Update routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  root "users#index"
+  root 'users#index'
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      post 'like', on: :member
+    end
   end
 end
