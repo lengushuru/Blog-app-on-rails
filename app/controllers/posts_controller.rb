cd9318cb53f09f0c9790a459b8d376fc3c9e361d
lengushuru <charleslengushuru@gmail.com>
2023-07-01 16:13:01 +0300

    Create comment and post for new user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,42 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments)
+    @posts = @user.posts
+    @current_user = current_user
   end
 
   def show
     @post = Post.find(params[:id])
-    @user = @post.author_id
-    @comments = @post.comments
+    @current_user = current_user
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def like
+    @post = Post.find(params[:id])
+    @like = Like.new(author_id: params[:user_id], post_id: @post.id)
+    @like.save
+    redirect_to user_post_path
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.author_id = current_user.id
+    @post.comments_counter = 0
+    @post.likes_counters = 0
+
+    if @post.save
+      redirect_to user_posts_path, notice: 'Post was successfully created.'
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
